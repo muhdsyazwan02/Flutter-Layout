a3ace0555f4e99925c01a6a8accab2536ede490e
ibnuazman <ibnuazman.11@gmail.com>
2022-01-01 02:48:45 +0800

    Edit some part on Part 1. Manage to toggle the widget

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is the assignment 2 GitHub for CSC14311 Mobile Application section 1 on Semester 1 2021/2022 KICT IIUM Gombak
 
 ## How to run the file :
-1- Install the main.dart file
+1- Install the project folder
 2- Create a new project with command "flutter create"
-3- Copy all the file related (main.dart, assets folder and pubspec.yaml) into the new directory file that have been created (substitute it)
+3- Copy all the file related (lib, assets folder and pubspec.yaml) into the new directory file that have been created (substitute it)
 4- Make sure to run your AVD first before test the code using command "flutter run"
--- fluttergridpart1/lib/main.dart
+++ fluttergridpart1/lib/main.dart
@@ -3,44 +3,41 @@ import 'package:fluttergridpart1/devices.dart';
 
 void main() => runApp(MyApp());
 
-/*class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}*/
-
 class MyApp extends StatelessWidget {
-  //String devicestate = "OFF";
-
-  /*void changeText() {
-    setState(() {
-      devicestate = "ON";
-    });
-  }*/
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.lightBlue,
-          title: const Text("MY IOT DEVICES"),
-        ),
-        body: Container(
-          color: Colors.grey[200],
-          child: GridView.count(
-            crossAxisCount: 2,
-            childAspectRatio: 1.7,
-            children: const <Widget>[
-              Devices("TV", "assets/images/tv.png", "OFF"),
-              Devices("Fan", "assets/images/fan.png", "OFF"),
-              Devices("Plug", "assets/images/plug.png", "Off"),
-              Devices("Light", "assets/images/light.png", "OFF"),
-              Devices("Aircond", "assets/images/aircond.png", "OFF"),
-              Devices("Wifi", "assets/images/wifi.png", "OFF"),
-            ],
-          ),
+      title: "MY IOT DEVICES",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MY IOT DEVICES"),
+      ),
+      body: Container(
+        color: Colors.grey[200],
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 1.7,
+          children: [
+            Devices("TV", "assets/images/tv.png"),
+            Devices("Fan", "assets/images/fan.png"),
+            Devices("Plug", "assets/images/plug.png"),
+            Devices("Light", "assets/images/light.png"),
+            Devices("Aircond", "assets/images/aircond.png"),
+            Devices("Wifi", "assets/images/wifi.png"),
+          ],
         ),
       ),
     );
